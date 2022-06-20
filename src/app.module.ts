22dd134c6ae88ff28357ab45fd312776fcfe6813
Baricho <ekabue0@gmail.com>
2022-06-20 14:58:59 +0300

    Add product schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose'; // 1.1 Import the mongoose mo
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module'; // 2.1 Import the product module
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/store'), // 1.2 Setup the database
-    ProductModule, // 2.2 Add the product module
+    ProductModule, UserModule, // 2.2 Add the product module
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+// import { Role } from 'src/auth/enums/role.enum';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop()
+  username: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
